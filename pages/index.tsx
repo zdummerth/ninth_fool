40a74f03108ec63fe799aab933ca3e6d449c265a
zach <zdummerth@gmail.com>
2022-12-28 08:55:36 -0600

    clean index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,28 @@
 import { supabaseAdmin } from '@/utils/supabase-admin';
 import { GetStaticPropsResult } from 'next';
 import { useUser } from 'utils/useUser';
-
 import Image from 'next/image';
 import Link from 'next/link';
 import mobilebg from 'public/mobile-bg.png';
-// import desktopbg from 'public/desktop-bg.png';
 import desktopbg from 'public/bg.svg';
 
 interface Props {
-  publicImages: any;
+  publicImages: string[];
 }
 
 export default function HomePage({ publicImages }: Props) {
-  // console.log(publicImages);
   const { user, subscription, isLoading } = useUser();
 
   const linkClassName =
     'block p-2 my-8 w-64 rounded-xl text-center shadow shadow-white bg-emerald-500 color black';
+
   return (
     <div>
-      {/* <Pricing products={products} /> */}
-      {/* <h1>The Ninth Fool</h1> */}
       <div className="lg:hidden">
         <Image src={mobilebg} layout="responsive" placeholder="blur" />
       </div>
       <div className="hidden lg:block">
-        <Image src={desktopbg} layout="responsive" 
-        // placeholder="blur" 
-        />
+        <Image src={desktopbg} layout="responsive" priority />
       </div>
       <div className="grid grid-cols-2 gap-4 lg:grid-cols-4 mx-2">
         {publicImages.map((i: string) => {
@@ -64,27 +58,23 @@ export default function HomePage({ publicImages }: Props) {
 }
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
-  // const products = await getActiveProductsWithPrices();
-  const { data, error } = await supabaseAdmin.storage
-    .from('public-images')
-    .list('', {
-      limit: 100,
-      offset: 0
-    });
+  const { data } = await supabaseAdmin.storage.from('public-images').list('', {
+    limit: 100,
+    offset: 0
+  });
 
   if (data) {
     const images = data
       .filter((img) => img.name !== '.emptyFolderPlaceholder')
-      .map((i) => {
-        // console.log(i);
-        return `https://iyepowyaftcjvbuntjts.supabase.co/storage/v1/object/public/public-images/${i.name}`;
-      });
+      .map(
+        (i) =>
+          `https://iyepowyaftcjvbuntjts.supabase.co/storage/v1/object/public/public-images/${i.name}`
+      );
 
     return {
       props: {
         publicImages: images
       }
-      // revalidate: 60
     };
   } else {
     return {
